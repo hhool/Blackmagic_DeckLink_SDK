132aba7efc0a5d21daa6ebc131bbc9c9622519fa
hhool <hhool.student@sina.com>
2016-06-17 12:01:52 +0800

    Blackmagic DeckLink SDK 10.6.6

---
+++ Win/Samples/GdiKeyer/GdiKeyer.h
@@ -0,0 +1,23 @@
+
+#include "DeckLinkAPI_h.h"
+
+// The callback class is used for video input format detection in this example.
+class DeckLinkKeyerDelegate : public IDeckLinkInputCallback
+{
+private:
+	ULONG		m_refCount;
+
+	~DeckLinkKeyerDelegate();
+	
+public:
+	DeckLinkKeyerDelegate();
+
+	// IUnknown
+	virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID iid, LPVOID *ppv);
+	virtual ULONG STDMETHODCALLTYPE AddRef(void);
+	virtual ULONG STDMETHODCALLTYPE  Release(void);
+
+	// IDeckLinkInputCallback
+	virtual HRESULT STDMETHODCALLTYPE VideoInputFormatChanged(BMDVideoInputFormatChangedEvents, IDeckLinkDisplayMode*, BMDDetectedVideoInputFormatFlags);
+	virtual HRESULT STDMETHODCALLTYPE VideoInputFrameArrived(IDeckLinkVideoInputFrame*, IDeckLinkAudioInputPacket*);
+};
